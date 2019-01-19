bb1a9cdb46865854ade2616bc622e06a03bb8011
Mark <meichangliang@outlook.com>
2019-01-20 06:52:46 +0800

    ~

---
+++ hello_world/target/debug/deps/hello_world-31caae3348a91e76.d
@@ -0,0 +1,5 @@
+C:\Mark\MyProject\rust_study\hello_world\target\debug\deps\hello_world-31caae3348a91e76.exe: src\main.rs
+
+C:\Mark\MyProject\rust_study\hello_world\target\debug\deps\hello_world-31caae3348a91e76.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Mark\MyProject\rust_study\hello_world\target\debug\hello_world.pdb: C:\Mark\MyProject\rust_study\hello_world\src\main.rs
