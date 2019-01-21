4beb91590c96c04e3d83dc2e1aff181b780fad13
Mark <meichangliang@outlook.com>
2019-01-21 23:18:34 +0800

    add

---
+++ basics/loops/loops.rs
@@ -0,0 +1,19 @@
+fn main() {
+    //无限循环
+    // loop {
+    //     println!("Loop forever!");
+    // }
+
+
+    //while
+    // let mut x = 5; // mut x: i32
+    // let mut done : bool = false; // mut done: bool
+
+    // while !done {
+    //     x += x - 3;
+    //     println!("{}", x);
+    //     if x % 5 == 0 {
+    //         done = true;
+    //     }
+    // }
+}
