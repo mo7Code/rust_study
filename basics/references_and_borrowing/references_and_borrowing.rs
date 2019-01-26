b522d43a6ffe6f11b6539ac383f9e8e6227e8356
Mark <meichangliang@outlook.com>
2019-01-26 19:08:07 +0800

    add

--- basics/references_and_borrowing/references_and_borrowing.rs
+++ basics/references_and_borrowing/references_and_borrowing.rs
@@ -1,4 +1,21 @@
 fn main() {
-    //引用和借用
-    println!("引用和借用")
+    // Don't worry if you don't understand how `fold` works, the point here is that an immutable reference is borrowed.
+    fn sum_vec(v: &Vec<i32>) -> i32 {
+        return v.iter().fold(0, |a, &b| a + b);
+    }
+    // Borrow two vectors and sum them.
+    // This kind of borrowing does not allow mutation through the borrowed reference.
+    fn foo(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
+        // Do stuff with `v1` and `v2`.
+        let s1 = sum_vec(v1);
+        let s2 = sum_vec(v2);
+        // Return the answer.
+        s1 + s2
+    }
+
+    let v1 = vec![1, 2, 3];
+    let v2 = vec![4, 5, 6];
+
+    let answer = foo(&v1, &v2);
+    println!("{}", answer);
 }
